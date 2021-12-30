948110862ce6b4cb69522afda8b64fc55219380b
Henry Asiedu <henryasiedu96@gmail.com>
2021-12-30 02:40:28 +0000

    remove feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@ import { Cage } from "@synevix/react-widget";
 import Slide from "../components/Carousel";
 import Service from "../components/Service";
 import Category from '../components/Category';
-import Feature from '../components/Feature'; 
+// import Feature from '../components/Feature'; 
 
 const Home = () => (
   <Cage className="mb-6">
     <Slide />
     <Service />
     <Category />
-    <Feature/> 
+    {/* <Feature/>  */}
   </Cage>
 
 )
